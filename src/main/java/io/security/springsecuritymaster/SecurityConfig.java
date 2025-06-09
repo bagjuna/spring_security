772e798015405898e70baebfb505d41289ad8039
bagjuna <juna.park124@gmail.com>
2025-06-09 15:18:30 +0900

    rememberMeAuthenticationFilter

--- src/main/java/io/security/springsecuritymaster/SecurityConfig.java
+++ src/main/java/io/security/springsecuritymaster/SecurityConfig.java
@@ -2,6 +2,7 @@ package io.security.springsecuritymaster;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -18,9 +19,17 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
-                .authorizeHttpRequests( auth -> auth.anyRequest().authenticated())
-                .httpBasic(basic -> basic
-                        .authenticationEntryPoint(new CustomAuthenticationEntryPoint())
+                .authorizeHttpRequests
+                        ( auth -> auth.anyRequest().authenticated())
+                .formLogin(Customizer.withDefaults())
+
+                .rememberMe(rememberMe -> rememberMe
+                        .alwaysRemember(true)
+                        .tokenValiditySeconds(3600) // 토큰 유효 시간 (초 단위)
+                        .userDetailsService(userDetailsService())
+                        .rememberMeParameter("remember") // 로그인 시 사용자를 기억하기 위해 사용되는 HTTP 매개변수
+                        .rememberMeCookieName("remember") // 기억하기 인증을 위한 토큰을 저장하는 쿠키 이름
+                        .key("security") // 기억하기 기능을 사용하기 위한 키
                 );
 
         return http.build();

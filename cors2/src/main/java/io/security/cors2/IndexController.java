037f6cc9583da7070a033652ded1a6b173a70e32
bagjuna <juna.park124@gmail.com>
2025-10-08 13:33:31 +0900

    v1

---
+++ cors2/src/main/java/io/security/cors2/IndexController.java
@@ -0,0 +1,17 @@
+package io.security.cors2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class IndexController {
+
+    @GetMapping("/users")
+    public String users() {
+        return "{\"name\":\"hong gil dong\"}";
+    }
+}
+
+

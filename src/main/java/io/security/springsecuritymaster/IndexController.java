71d36d0694a0395a22e11a2f3039d4848072d421
bagjuna <juna.park124@gmail.com>
2025-10-18 19:59:38 +0900

    완성 (#11)
    * 완성
    
    * 완성
    
    # Conflicts:
    #	src/main/java/io/security/springsecuritymaster/IndexController.java
    #	src/main/java/io/security/springsecuritymaster/SecurityConfig.java
    
    * 완성
    
    # Conflicts:
    #	src/main/java/io/security/springsecuritymaster/IndexController.java
    #	src/main/java/io/security/springsecuritymaster/SecurityConfig.java
    
    * 완성
    
    # Conflicts:
    #	src/main/java/io/security/springsecuritymaster/IndexController.java
    #	src/main/java/io/security/springsecuritymaster/SecurityConfig.java
    
    * 완성
    
    # Conflicts:
    #	src/main/java/io/security/springsecuritymaster/IndexController.java
    #	src/main/java/io/security/springsecuritymaster/SecurityConfig.java
    
    * 완성
    
    # Conflicts:
    #	src/main/java/io/security/springsecuritymaster/IndexController.java
    #	src/main/java/io/security/springsecuritymaster/SecurityConfig.java
    
    * 충돌 해결 2

--- src/main/java/io/security/springsecuritymaster/IndexController.java
+++ src/main/java/io/security/springsecuritymaster/IndexController.java
@@ -1,7 +1,15 @@
 package io.security.springsecuritymaster;
 
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
 public class IndexController {
@@ -25,4 +33,21 @@ public class IndexController {
         return "admin";
     }
 
+    @GetMapping("/login")
+    public String login(HttpServletRequest request, MemberDto memberDto) throws ServletException{
+        request.login(memberDto.getUsername(), memberDto.getPassword());
+        System.out.println("login is successful");
+        return "login";
+    }
+
+    @GetMapping("/users")
+    public List<MemberDto> users(HttpServletRequest request, HttpServletResponse response) throws
+		ServletException,
+		IOException {
+        boolean authenticate = request.authenticate(response);
+        if (authenticate) {
+            return List.of(new MemberDto("user","1111"));
+        }
+        return Collections.emptyList();
+    }
 }

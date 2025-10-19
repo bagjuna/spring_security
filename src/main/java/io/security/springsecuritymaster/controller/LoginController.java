830d7eb4c9dc5f674ced32b16f3f7aa3e1b536fb
bagjuna <juna.park124@gmail.com>
2025-10-20 02:20:26 +0900

    로그아웃 구현 완료

--- src/main/java/io/security/springsecuritymaster/controller/LoginController.java
+++ src/main/java/io/security/springsecuritymaster/controller/LoginController.java
@@ -1,8 +1,13 @@
 package io.security.springsecuritymaster.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
 public class LoginController {
@@ -14,6 +19,20 @@ public class LoginController {
     @GetMapping("/signup")
     public String signup() {
         return "login/signup";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        Authentication authentication = SecurityContextHolder.getContextHolderStrategy()
+            .getContext()
+            .getAuthentication();
+
+        if(authentication != null){
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+        }
+
+        return "redirect:/login";
+
     }
 
 }

9d745365daecc8c98fd8cf94810fd717c8ca3276
bagjuna <juna.park124@gmail.com>
2025-10-21 03:36:36 +0900

    rest 인증 필터 구현

--- src/main/java/io/security/springsecuritymaster/security/config/SecurityConfig.java
+++ src/main/java/io/security/springsecuritymaster/security/config/SecurityConfig.java
@@ -4,15 +4,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationDetailsSource;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.WebAuthenticationDetails;
 
+import io.security.springsecuritymaster.security.filters.RestAuthenticationFilter;
 import io.security.springsecuritymaster.security.handler.FormAccessDeniedHandler;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
@@ -58,13 +62,20 @@ public class SecurityConfig {
 	@Bean
 	@Order(1)
 	public SecurityFilterChain restFilterChain(HttpSecurity http) throws Exception {
+
+		AuthenticationManager authenticationManager = http.getSharedObject(
+				AuthenticationManagerBuilder.class)
+			.authenticationProvider(authenticationProvider)
+			.build();
+
 		http
 			.authorizeHttpRequests(auth -> auth
 				.requestMatchers("/css/**", "/images/**", "/js/**", "/favicon.*", "/*/icon-*").permitAll()
 				.anyRequest().permitAll()
 			)
 			.csrf(AbstractHttpConfigurer::disable)
-
+			.addFilterBefore(restAuthenticationFilter(authenticationManager), UsernamePasswordAuthenticationFilter.class)
+			.authenticationManager(authenticationManager)
 
 		;
 
@@ -72,6 +83,10 @@ public class SecurityConfig {
 		return http.build();
 	}
 
-
+	private RestAuthenticationFilter restAuthenticationFilter(AuthenticationManager authenticationManager) {
+		RestAuthenticationFilter filter = new RestAuthenticationFilter();
+		filter.setAuthenticationManager(authenticationManager);
+		return filter;
+	}
 
 }

6055649e9b3ef7c822a8ab2b459790a5c77e734d
bagjuna <juna.park124@gmail.com>
2025-10-20 22:46:13 +0900

    rest 인증 보안 및 화면구성

--- src/main/java/io/security/springsecuritymaster/security/config/SecurityConfig.java
+++ src/main/java/io/security/springsecuritymaster/security/config/SecurityConfig.java
@@ -2,10 +2,12 @@ package io.security.springsecuritymaster.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationDetailsSource;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -28,6 +30,7 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
+			.securityMatcher("/api/login")
 			.authorizeHttpRequests(auth -> auth
 				.requestMatchers("/css/**", "/images/**", "/js/**", "/favicon.*", "/*/icon-*").permitAll()
 				.requestMatchers("/", "/signup", "/login*").permitAll()
@@ -52,7 +55,22 @@ public class SecurityConfig {
 		return http.build();
 	}
 
+	@Bean
+	@Order(1)
+	public SecurityFilterChain restFilterChain(HttpSecurity http) throws Exception {
+		http
+			.authorizeHttpRequests(auth -> auth
+				.requestMatchers("/css/**", "/images/**", "/js/**", "/favicon.*", "/*/icon-*").permitAll()
+				.anyRequest().permitAll()
+			)
+			.csrf(AbstractHttpConfigurer::disable)
 
+
+		;
+
+
+		return http.build();
+	}
 
 
 

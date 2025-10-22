45dca65f5efb3c9eb77e222b36817e172321e851
bagjuna <juna.park124@gmail.com>
2025-10-22 17:19:14 +0900

    회원 관리 시스템 - 메모리 기반 완성

--- src/main/java/io/security/springsecuritymaster/security/config/SecurityConfig.java
+++ src/main/java/io/security/springsecuritymaster/security/config/SecurityConfig.java
@@ -6,11 +6,13 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationDetailsSource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authorization.AuthorizationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.WebAuthenticationDetails;
 
@@ -23,6 +25,7 @@ import io.security.springsecuritymaster.security.handler.FormAuthenticationSucce
 import io.security.springsecuritymaster.security.handler.RestAccessDeniedHandler;
 import io.security.springsecuritymaster.security.handler.RestAuthenticationFailureHandler;
 import io.security.springsecuritymaster.security.handler.RestAuthenticationSuccessHandler;
+import io.security.springsecuritymaster.security.manager.CustomDynamicAuthorizationManager;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 
@@ -38,32 +41,27 @@ public class SecurityConfig {
 	private final FormAuthenticationFailureHandler failureHandler;
 	private final RestAuthenticationSuccessHandler restSuccessHandler;
 	private final RestAuthenticationFailureHandler restFailureHandler;
-
+	private final AuthorizationManager<RequestAuthorizationContext> authorizationManager;
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
 			.authorizeHttpRequests(auth -> auth
-				.requestMatchers("/css/**", "/images/**", "/js/**", "/favicon.*", "/*/icon-*").permitAll()
-				.requestMatchers("/", "/signup", "/login*").permitAll()
-				.requestMatchers("/user").hasAuthority("ROLE_USER")
-				.requestMatchers("/manager").hasAuthority("ROLE_MANAGER")
-				.requestMatchers("/admin").hasAuthority("ROLE_ADMIN")
-				.anyRequest().permitAll())
+				.anyRequest().access(authorizationManager)
+			)
 			.formLogin(form -> form
 				.loginPage("/login")
 				.authenticationDetailsSource(authenticationDetailsSource)
 				.successHandler(successHandler)
 				.failureHandler(failureHandler)
-				.permitAll()
-			)
-			//        .csrf(AbstractHttpConfigurer::disable)
+				.permitAll())
 			.authenticationProvider(authenticationProvider)
-			.exceptionHandling(exception ->
-				exception.accessDeniedHandler(new FormAccessDeniedHandler("/denied")));
-
-
-
+			.exceptionHandling(exception -> exception
+				.accessDeniedHandler(new FormAccessDeniedHandler("/denied"))
+			)
+		;
 		return http.build();
+
+
 	}
 
 	@Bean
